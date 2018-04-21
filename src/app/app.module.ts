1e380418f5bd01888ad3a7a5189eb260fb20af28
robertrayburn <robcrayb@ut.utm.edu>
2018-04-20 22:31:24 -0500

    Adding in all the basic stuff for angular project. :ok_hand:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// ADD THE FIREBASE IMPORTS
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+//
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'AngularResturauntRoulette-dev'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAfKw0tB0QAXBU1DbrRxP5ugFRP8NGnXKE",
+    authDomain: "restaurant-roulette-6f286.firebaseapp.com",
+    databaseURL: "https://restaurant-roulette-6f286.firebaseio.com",
+    projectId: "restaurant-roulette-6f286",
+    storageBucket: "restaurant-roulette-6f286.appspot.com",
+    messagingSenderId: "1063202666057"
+  }
 };
